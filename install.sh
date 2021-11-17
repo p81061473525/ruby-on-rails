c8c778c390baf8f101bf86c84620ce30ea6ae88a
Dograbbit <p81061473525@gmail.com>
2021-11-17 10:04:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 cd ~
 command curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --import -
 command curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg2 --import -
+\curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm install ruby
 ruby -v
